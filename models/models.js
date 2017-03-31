5ed6b75f6bbc11302e0f5df494a444d8e23db8c3
Shivam Mathur <shivam-pt1413@csez.zohocorpin.com>
2017-03-31 19:48:50 +0530

    moved to sockets

---
+++ models/models.js
@@ -0,0 +1,38 @@
+var ObjectID = require('mongodb').ObjectID;
+var objectId = new ObjectID();
+var offset = +5.5;
+var models = {
+	
+	HouseHold:{
+		_id:"",
+		name:"",
+		controllers: [],
+		tasks:[
+			{
+				controllerId:"",
+				date: "",
+				task:false,
+				duration:0
+			}
+		],
+		connected:false,
+		sync:false,
+		created:"",
+		modified:""
+		
+	},
+	controller:{
+		_id:"",
+		name:"",
+		status:false,
+		created:"",
+		modified:""
+	}.
+	newObjectId: function(){
+		return objectId.toHexString();
+	},
+	getDateTime: function(){
+		return (new Date( new Date().getTime() + offset * 3600 * 1000).toUTCString().replace( / GMT$/, "" ));
+	}
+};
+module.exports = models;
--- routes/index.js
+++ routes/index.js
@@ -1,157 +1,171 @@
 var express = require('express');
 var request = require('request');
 var path = require('path');
+var LOG = require('log');
+var log  = new LOG();
 var router = express.Router();
 var MongoApi = require(path.join(__dirname+"/../db/db"));
+var model = require(path.join(__dirname+"/../models/models"));
+var io = require('socket.io')(server);
+var currentConnections  = {};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/main',function(req,res,next){
-	var onConnect = function(err,db){
+	let onConnect = (err,db)=>{
 		if(err){
-			console.log(err);
-			res.render("main",{err:true,errMessage:"db connect error",doc:null});
+			log.error(err);
+			res.send("Error connecting to database");
 		}else{
-			var collection = db.collection("cmd");
-			collection.find(function(err,cursor){
-				if(err){
-					console.log(err);
-					res.render("main",{err:true,errMessage:"Find error",doc:null});
+			let collection = db.collection("house_hold");
+			collection.find().toArray().then((house_holds)=>{
+				let data = [];
+				if(house_holds){
+					data = house_holds;
 				}else{
-					cursor.toArray().then(function(data){
-						if(data){res.render("main",{err:false,errMessage:"null",doc:data});}else{console.log("No data");}
-					});
+					log.error("No house holds");
 				}
+				res.render('index',{houses:data});
 			});
 		}
 	};
 	MongoApi.connect(onConnect);
 });
-router.get('/addCmdToQueue',function(req,res,next){
-	var params = req.query;
-	var id = parseInt(params.id);
-	var onConnect=function(err,db){
-		if(err){
-			console.log(err);
-			res.status(500).send("Db error");
-		}else{
-			var collection = db.collection("cmd");
-			var query = {
-				'_id':id
-			};
-			collection.findOne(query,function(err,doc){
-				if(err){
-					console.log(err);
-					res.status(500).send("Find error");
-				}else{
-					doc.cmd.push(params.cmd);
-					var cmd = doc.cmd;
-					var onUpdate = function(err){
-						if(err){
-							console.log(err);
-							res.status(500).send("Update error");
-						}else{
-							res.status(200).send("CMD Updated, Current queue: "+ cmd);
-						}
-					};
-					collection.update(query,doc,onUpdate);
-					
-					db.close()
-				}
-			});
-		}
-	
-	}
-	MongoApi.connect(onConnect);
-	//console.log(db);
-	/*db.collection('cmds').findOne({},function(err,doc){
-		if(err){console.log(err);}else{console.log(doc);}
-	});
-	console.log(params);*/
-});
-router.get('/fetchTask',function(req,res,next){
-	var params = req.query;
-	var id = parseInt(params.id);
-	var onConnect=function(err,db){
-		if(err){
-			console.log(err);
-			res.status(500).send("Db error");
-		}else{
-			var collection = db.collection("cmd");
-			var query = {
-				'_id':id
-			};
-			collection.findOne(query,function(err,doc){
-				if(err){
-					console.log(err);
-					db.close();
-					res.status(500).send("Find error");
-				}else{
-					var cmd = doc.cmd;
-					res.status(200).send(cmd);
-					
-					db.close()
-				}
-			});
-		}
-	
-	}
-	MongoApi.connect(onConnect);
-	//console.log(db);
-	/*db.collection('cmds').findOne({},function(err,doc){
-		if(err){console.log(err);}else{console.log(doc);}
-	});
-	console.log(params);*/
-});
-router.get('/init',function(req,res,next){
-	var params = req.query;
-	var id = parseInt(params.id);
-	var onConnect = function(err,db){
-		if(err){
-			console.log(err);
-			res.status(500).send("db connection error");
-		}else{
-			var collection = db.collection("cmd");
-			var query = {
-				'_id':id
-			};
-			var onFind = function(err,doc){
-				if(err){
-					console.log(err);
-					res.status(500).send("Find error");
-				}else{
-					if(doc){
-						var ip = req.connection.remoteAddress;
-						console.log(ip);
-						doc.latestIP = ip;
-						
-						var onUpdate = function(err){
-						if(err){
-							console.log(err);
-							res.status(500).send("Update error");
-						}else{
-							res.status(200).send("Ip updated "+ ip);
-						}
-					};
-					collection.update(query,doc,onUpdate);
+io.on('connection',(socket)=>{
+	socket.emit('connected',{connected: true});
+	socket.on('init',(data)=>{
+		let onConnect = (err,db)=>{
+			if(err){
+				log.error(err);
+				socket.emit("error_db",{reconnect:true});
+			}else{
+				let collection = db.collection("house_hold");
+				let house = model.HouseHold;
+				house._id = model.newObjectId();
+				house.created = model.getDateTime();
+				house.modified = model.getDateTime();
+				house.name = data.name;
+				collection.insert(house,(err,doc)=>{
+					if(err){
+						log.error(err);
+						socket.emit("error_init",{reconnect:true});
+					}else{
+						log.info("Init : " + house._id);
+						socket.emit("sucess_init",house);
 					}
-					else {
-						collection.insert({'_id':id,'cmd':[],'latestIP':req.connection.remoteAddress},function(err,doc){
-							if(err){
-								console.log(err);
-								res.status(500).send("insert failed");
-							}else{
-								console.log("Init Success");
-								res.status(200).send("insert success");
-							}
-						});
-					}
-				}
+				});
 			}
-			collection.findOne(query,onFind);
+		};
+		MongoApi.connect(onConnect);
+	});
+	socket.on('reconnect',(data)=>{
+		if(data._id){
+			let onConnect = (err,db)=>{
+				if(err){
+					log.error(err);
+					socket.emit("error_db",{reconnect:true});
+				}else{
+					let collection = db.collection("house_hold");
+					collection.findOne({_id:data._id},(err,doc)=>{
+						if(err){
+							log.error(err);
+							socket.emit("error_find",{reconnect:true});
+						}else{
+							if(doc){
+								doc.connected = true;
+								doc.modified = model.getDateTime();
+								collection.update({_id:data._id},doc,(err,d)=>{
+									if(err){
+										log.error(err);
+										socket.emit("error_update",{reconnect:true});
+									}else{
+										currentConnections[socket.id] = data._id;
+										log.info("connected : " + data._id);
+										socket.emit('success_connect',{reconnect:false});
+									}
+								});
+							}else{
+								log.error("Not found : "+ data._id);
+								socket.emit("error",{reconnect:true});	
+							}
+						}
+					});
+					
+				}
+			};
+			MongoApi.connect(onConnect);
+		}else{
+			socket.emit("error_data",{reconnect:true});
 		}
-	}
-	MongoApi.connect(onConnect);
-})
+	});
+	socket.on('sync',(data)=>{
+		if(data.modified){
+			MongoApi.connect((err,db)=>{
+				if(err){
+					log.error(err);
+					socket.emit("error_db",{reconnect:true});
+				}else{
+					let collection = db.collection("house_hold");
+					collection.findOne({_id:data._id},(err,doc)=>{
+						if(err){
+							log.error(err);
+							socket.emit("error_find");
+						}else{
+							if(doc){
+								var rpiDate = new Date(data.modified);
+								var serverDate = new Date(doc.modified);
+								if(rpiData>serverData){
+									doc.controllers = data.controllers;
+									doc.tasks = data.tasks;
+									doc.sync = true;
+									doc.modified = model.getDateTime();
+									collection.update({_id:doc._id},doc,(err,d)=>{
+										if(err){
+											log.error(err);
+											socket.emit("error_update",{reconnect:true});
+										}else{
+											currentConnections[socket.id] = data._id;
+											log.info("Synced : " + data._id);
+											socket.emit('success_sync',{reconnect:false});
+										}
+									});
+								}else{
+									socket.emit('need_sync',doc);
+								}
+							}else{
+								log.error("Not found : "+ data._id);
+								socket.emit("error",{reconnect:true});	
+							}
+						}
+					});
+				}
+			});
+		}else{
+			socket.emit("error_data",{reconnect:true});
+		}
+	});
+	socket.on("disconnect",(data)=>{
+		MongoApi.connect((err,db)=>{
+			if(err){
+				log.error(err);
+			}else{
+				let collection = db.collection("house_hold");
+				let id = currentConnections[socket.id];
+				collection.findOne({_id:id},(err,doc)=>{
+					if(err){
+						log.error(err);
+					}else{
+						if(doc){
+							doc.connected = false;
+							doc.modified = model.getDateTime;
+							collection.update({_id:id},doc,(err,d)=>{
+								if(err){
+									log.error(err);
+								}
+							});
+						}
+					}
+				});
+			}
+		});
+	});
+});
 module.exports = router;
