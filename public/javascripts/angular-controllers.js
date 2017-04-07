1739fe3dd2db9c7eb691600479ea75c1dc160413
BurnUalive <burnualive@hotmail.com>
2017-04-07 19:43:57 +0530

    testing

--- public/javascripts/angular-controllers.js
+++ public/javascripts/angular-controllers.js
@@ -12,6 +12,33 @@ controllers.main = function($scope,$http){
 	$scope.setup =function(){
 		houses($scope,$http);
 	};
-	//$scope.toggelSwitch = test;
+	
+function test(house){
+		console.log(house);
+		var controllerId = $("#"+house._id).find("select").val();
+		console.log(controllerId);
+		if(controllerId!="None"){
+			let controller = house.controllers[controllerId];
+			let url = "";
+			if(controller.status){
+				url = "/house/turnOn";
+			}else{
+				url = "/house/turnOff";
+			}
+			console.log(url);
+			$http({
+				url:url,
+				method:'POST',
+				data:{
+					house:house,
+					controller:controller
+				}
+			}).then((data)=>{
+				console.log(data);
+			});
+		}
+		
+	}
+	$scope.toggelSwitch = test;
 };
 app.controller(controllers);
